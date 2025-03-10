50740a742e357c5d1d51ab8ff5cb7a8fbd570d4a
Juan Miguel León Gómez <85261235+Juanm0331123@users.noreply.github.com>
2025-03-09 19:47:56 -0500

    Agregar funcionalidad de pago y manejo de webhooks, incluir archivo HTML para la interfaz de usuario y configurar archivos ignorados

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -24,12 +24,12 @@ export const createOrder = async (req, res) => {
           failure: "http://localhost:3000/failure",
           pending: "http://localhost:3000/pending",
         },
-        notification_url: "https://7041-190-1-208-152.ngrok-free.app/webhook",
+        notification_url: "https://153b-190-1-215-48.ngrok-free.app/webhook",
       },
     });
 
     console.log(result);
-    res.json(result.body);
+    res.json({ data: result.body || result });
   } catch (error) {
     console.error(error);
     res.status(500).send("Error al crear la orden");
@@ -37,5 +37,35 @@ export const createOrder = async (req, res) => {
 };
 
 export const receiveWebhook = async (req, res) => {
-  res.send("Recibiendo Webhook");
+  const payment = req.query;
+
+  try {
+    if (payment.type === "payment") {
+      const paymentId = payment["data.id"];
+
+      const response = await fetch(
+        `https://api.mercadopago.com/v1/payments/${paymentId}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${MERCADO_PAGO_ACCESS_TOKEN}`,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`Error en la respuesta: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log("Datos del pago:", data);
+
+      // Se guarda el pago en la base de datos
+    }
+
+    res.sendStatus(204);
+  } catch (error) {
+    console.error("Error al recibir el webhook:", error);
+    res.status(500).send("Error al recibir el webhook: " + error.message);
+  }
 };
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import express from "express";
 import morgan from "morgan";
 import PaymentRoutes from "./routes/payments.routes.js";
 import { PORT } from "./config.js";
+import path from "path";
 
 const app = express();
 
 app.use(morgan("dev"));
 
 app.use(PaymentRoutes);
+
+app.use(express.static(path.resolve("src/public")));
 
 app.listen(PORT, () => {
   console.log("Server is running on port 3000");
